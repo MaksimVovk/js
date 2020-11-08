b73a7d92077b90111c87b5c391feb445806eb5a4
MaximVovk <maksemvovk@gmail.com>
2020-11-08 23:28:17 +0200

    typescript: added some improvements

--- typescript-cource/using-ts.js
+++ typescript-cource/using-ts.js
@@ -1,11 +1,17 @@
 var button = document.querySelector('button');
+var body = document.querySelector('body');
 var input1 = document.getElementById('num1');
 var input2 = document.getElementById('num1');
 function add(num1, num2) {
     return num1 + num2;
 }
-button.addEventListener("click", function () {
+button.addEventListener('click', function () {
+    var d = new Date();
+    var datestring = d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes();
     var num1 = +input1.value;
     var num2 = +input2.value;
-    console.log(add(num1, num2));
+    var elem = document.createElement('div');
+    elem.style.cssText = 'display: grid; grid-template-columns: 50px 200px 100px; grid-gap: 10px;';
+    elem.innerHTML = "<div>" + 1 + "</div><div>" + datestring + "</div><div>" + (num1 + num2) + "</div>";
+    body.appendChild(elem);
 });
--- typescript-cource/using-ts.ts
+++ typescript-cource/using-ts.ts
@@ -1,4 +1,5 @@
 const button = document.querySelector('button')
+const body = document.querySelector('body')
 const input1 = document.getElementById('num1')! as HTMLInputElement
 const input2 = document.getElementById('num1')! as HTMLInputElement
 
@@ -6,8 +7,15 @@ function add (num1: number, num2: number) {
 	return num1 + num2
 }
 
-button.addEventListener("click", function () {
+button.addEventListener('click', function () {
+	const d = new Date()
+	const datestring = d.getDate()  + "/" + (d.getMonth()+1) + "/" + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes()
+
 	const num1 = +input1.value
 	const num2 = +input2.value
-	console.log(add(num1, num2))
+
+	const elem = document.createElement('div')
+	elem.style.cssText = 'display: grid; grid-template-columns: 50px 200px 100px; grid-gap: 10px;'
+	elem.innerHTML = `<div>${1}</div><div>${datestring}</div><div>${num1 + num2}</div>`
+	body.appendChild(elem)
 })
