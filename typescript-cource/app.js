7534ddcc48e0dd67b79d51d1a77c25aaeddb28d4
MaximVovk <maksemvovk@gmail.com>
2020-11-11 10:15:38 +0200

    typescript: Object Types

--- typescript-cource/app.js
+++ typescript-cource/app.js
@@ -1,12 +1,5 @@
-function add(n1, n2, showResult, phraze) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phraze + result);
-    }
-    return result;
-}
-var num1 = 5;
-var num2 = 2.8;
-var printResult = true;
-var resultPhraze = 'Result is: ';
-add(num1, num2, printResult, resultPhraze);
+var person = {
+    name: 'Maxim',
+    age: 25
+};
+console.log(person.name);
--- typescript-cource/app.ts
+++ typescript-cource/app.ts
@@ -1,16 +1,9 @@
-function add(n1: number, n2: number, showResult: boolean, phraze: string) {
-  const result = n1 + n2
-
-  if (showResult) {
-    console.log(phraze + result)
-  }
-
-  return result
+const person: {
+  name: string
+  age: number
+} = {
+  name: 'Maxim',
+  age: 25
 }
 
-const num1 = 5
-const num2 = 2.8
-const printResult = true
-const resultPhraze = 'Result is: '
-
-add(num1, num2, printResult, resultPhraze)
+console.log(person.name)
