1920081ea800f1daa90af75141f497f623483735
MaximVovk <maksemvovk@gmail.com>
2020-11-11 10:26:02 +0200

    typescript: Array Types

--- typescript-cource/app.js
+++ typescript-cource/app.js
@@ -1,5 +1,12 @@
 var person = {
     name: 'Maxim',
-    age: 25
+    age: 25,
+    hobbies: ['Sport', 'Cooking']
 };
+var favoritesActivities;
+favoritesActivities = ['Sport'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- typescript-cource/app.ts
+++ typescript-cource/app.ts
@@ -1,9 +1,18 @@
 const person: {
   name: string
   age: number
+  hobbies: string[]
 } = {
   name: 'Maxim',
-  age: 25
+  age: 25,
+  hobbies: ['Sport', 'Cooking'],
 }
 
+let favoritesActivities: string[]
+favoritesActivities = ['Sport']
+
 console.log(person.name)
+
+for (const hobby of person.hobbies) {
+  console.log(hobby)
+}
