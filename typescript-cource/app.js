d87ac32dad09c35d90ad790593ab3e95eaa22e9b
MaximVovk <maksemvovk@gmail.com>
2020-11-26 00:36:01 +0200

    typescript: Function Return Types and Void

--- typescript-cource/app.js
+++ typescript-cource/app.js
@@ -1,21 +1,7 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if (input1 === 'number' && input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    // if (resultConversion === 'as-number') {
-    //   return +result
-    // } else {
-    //   return result.toString()
-    // }
-    return result;
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combineAges = combine(30, 26, 'as-number');
-console.log(combineAges);
-var combineNames = combine('Test', ' Tester', 'as-text');
-console.log(combineNames);
-var combineStringAges = combine('30', '26', 'as-number');
-console.log(combineStringAges);
+function printResult(num) {
+    console.log('Result: ', num);
+}
+printResult(add(5, 12));
--- typescript-cource/app.ts
+++ typescript-cource/app.ts
@@ -1,32 +1,9 @@
-type Combinable = number | string
-type CombinableDescriptor = 'as-number' | 'as-text'
-
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: CombinableDescriptor
-) {
-  let result
-
-  if (input1 === 'number' && input2 === 'number' || resultConversion === 'as-number') {
-    result = +input1 + +input2
-  } else {
-    result = input1.toString() + input2.toString()
-  }
-
-  // if (resultConversion === 'as-number') {
-  //   return +result
-  // } else {
-  //   return result.toString()
-  // }
-  return result
+function add (n1: number, n2: number): number {
+  return n1 + n2
 }
 
-const combineAges = combine(30, 26, 'as-number')
-console.log(combineAges)
+function printResult (num: Number): void {
+  console.log('Result: ', num)
+}
 
-const combineNames = combine('Test', ' Tester', 'as-text')
-console.log(combineNames)
-
-const combineStringAges = combine('30', '26', 'as-number')
-console.log(combineStringAges)
+printResult(add(5, 12))
