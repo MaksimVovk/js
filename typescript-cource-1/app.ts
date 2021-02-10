693440be6bc4d8aae10911687d06134ac6fca48f
MaximVovk <maksemvovk@gmail.com>
2021-02-11 00:52:58 +0200

    The "never" Type

--- typescript-cource-1/app.ts
+++ typescript-cource-1/app.ts
@@ -16,3 +16,10 @@ if (typeof userInput === 'string') {
 // let userInput: any
 // let userName: string
 //userName = userInput //not error because any is flexible
+
+// type "never" because never return anything
+function generateError (message: string, code: number): never {
+  throw { message: message, errorCode: code}
+}
+
+generateError('An error occurred!', 500)
