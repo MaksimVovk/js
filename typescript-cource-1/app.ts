6d6e6cbc2b60c8840c5f25d16bf6d0cfd3922f02
MaximVovk <maksemvovk@gmail.com>
2021-02-11 00:33:36 +0200

    Function Types & Callbacks

--- typescript-cource-1/app.ts
+++ typescript-cource-1/app.ts
@@ -14,7 +14,13 @@ function printResultU (num: number): undefined {
   return
 }
 
+function addAndHandle (n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2
+  cb(result)
+}
+
 printResult(add(5, 12))
+addAndHandle(24, 25, (result) => { console.log('RESULT CB', result) })
 
 // type "Function"
 // let combineValue: Function
