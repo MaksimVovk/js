765adb3251f2f1466ef305c66373de9926515245
MaximVovk <maksemvovk@gmail.com>
2021-02-14 00:00:04 +0200

    "private" and "public" Access Modifiers

--- typescript-cource-1/src/app.ts
+++ typescript-cource-1/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string
+  private employees: string[] = []
 
   constructor (n: string) {
     this.name = n
@@ -8,12 +9,25 @@ class Department {
   log (this: Department) {
     console.log('Department: ' + this.name)
   }
+
+  addEmployee (employee: string) {
+    this.employees = [...this.employees, employee]
+  }
+
+  printEmpleyessInformation () {
+    console.log(this.employees.length)
+    console.log(this.employees)
+  }
 }
 
 const accounting = new Department('Accounting')
 
 accounting.log()
+accounting.addEmployee('Max')
+accounting.addEmployee('Alex')
+// accounting.employees[2] = 'Manu' //Property 'employees' is private and only accessible within class 'Department'.
+accounting.printEmpleyessInformation()
 
-const accountingCopy = { name: 'Copy of Accounting', log: accounting.log }
+// const accountingCopy = { name: 'Copy of Accounting', log: accounting.log }
 
-accountingCopy.log()
+// accountingCopy.log()
