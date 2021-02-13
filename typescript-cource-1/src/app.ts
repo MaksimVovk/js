bf8ab338aef31f40abdfe8198cdcf4b20681d50f
MaximVovk <maksemvovk@gmail.com>
2021-02-14 00:06:17 +0200

    Shorthand Initialization

--- typescript-cource-1/src/app.ts
+++ typescript-cource-1/src/app.ts
@@ -1,13 +1,15 @@
 class Department {
-  name: string
+  // private id: string
+  // private name: string
   private employees: string[] = []
 
-  constructor (n: string) {
-    this.name = n
+  constructor (private id: string, public name: string) {
+    // this.id = id
+    // this.name = name
   }
 
   log (this: Department) {
-    console.log('Department: ' + this.name)
+    console.log(`Departmnet ${this.id}: ${this.name}`)
   }
 
   addEmployee (employee: string) {
@@ -20,7 +22,7 @@ class Department {
   }
 }
 
-const accounting = new Department('Accounting')
+const accounting = new Department('d1', 'Accounting')
 
 accounting.log()
 accounting.addEmployee('Max')
