f6b92818fb9a4ac0a51884be4e9ef9b6682211d3
MaximVovk <maksemvovk@gmail.com>
2021-02-13 16:41:50 +0200

    Compiling the Entire Project / Multiple

--- typescript-cource-1/Basics/basics.ts
+++ typescript-cource-1/Basics/basics.ts
@@ -1,4 +1,4 @@
-function add (n1: number, n2: number, showResult: boolean, phrase: string) {
+function addNumber (n1: number, n2: number, showResult: boolean, phrase: string) {
   const result = n1 + n2
 
   if (!showResult) {
@@ -10,7 +10,7 @@ function add (n1: number, n2: number, showResult: boolean, phrase: string) {
 
 const number1 = '1'
 const number2: number = 1.2
-const printResult = true
+const isPrintResult = true
 const phrase = 'Printing result inside function:'
 
-const result = add(+number1, +number2, printResult, phrase)
+const result = addNumber(+number1, +number2, isPrintResult, phrase)
---
+++ typescript-cource-1/extra.md
@@ -0,0 +1,8 @@
+# Watch mode
+
+> tsc app.ts -w
+
+# tsconfig.json file
+
+> tsc --init - *init "tsconfig.json" file*
+> tsc -w - *wach mode for all file in a project*
