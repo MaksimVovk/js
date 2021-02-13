85637a072cba4d0957a49437d73be0a6abd93233
MaximVovk <maksemvovk@gmail.com>
2021-02-13 23:27:22 +0200

    Creating a First Class

--- typescript-cource-1/src/app.ts
+++ typescript-cource-1/src/app.ts
@@ -1,1 +1,11 @@
-const app=  1
+class Department {
+  name: string
+
+  constructor (n: string) {
+    this.name = n
+  }
+}
+
+const accounting = new Department('Accounting')
+
+console.log(accounting)
