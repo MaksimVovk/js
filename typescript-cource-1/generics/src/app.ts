2035feed5f3ea7d46810ef7486b6137f3b3cd1d8
MaximVovk <maksemvovk@gmail.com>
2021-02-20 16:42:13 +0200

    Another Generic Function

--- typescript-cource-1/generics/src/app.ts
+++ typescript-cource-1/generics/src/app.ts
@@ -22,3 +22,22 @@ const mergeObject = merge({ name: 'Max', hobbies: ['Sport'] }, { age: 26 })
 
 console.log(mergeObject)
 
+interface Lengthy {
+  length: number
+}
+
+function countAndDescribe<T extends Lengthy> (element: T): [T, string] {
+  let descriptionText = 'Got no value.'
+
+  if (element.length === 1) {
+    descriptionText = 'Got 1 element'
+  } else if (element.length > 1) {
+    descriptionText = 'Got ' + element.length +  ' elements'
+  }
+
+  return [element, descriptionText]
+}
+
+console.log(countAndDescribe('Hi there!'))
+console.log(countAndDescribe(['Sport', 'Music']))
+console.log(countAndDescribe([]))
