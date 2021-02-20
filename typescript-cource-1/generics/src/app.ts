bfed705178a81bf9095fa151eedd173e2555551b
MaximVovk <maksemvovk@gmail.com>
2021-02-20 15:47:04 +0200

    Creating a Generic Function

--- typescript-cource-1/generics/src/app.ts
+++ typescript-cource-1/generics/src/app.ts
@@ -1,12 +1,24 @@
-const names: Array<string> = [] //string[]
-// names[0].split(' ')
+// const names: Array<string> = [] //string[]
+// // names[0].split(' ')
 
-const promise: Promise<number> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(10)
-  }, 200)
-})
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10)
+//   }, 200)
+// })
 
-promise.then(data => {
-  // data.split(' ')
-})
+// promise.then(data => {
+//   // data.split(' ')
+// })
+
+function merge<T, U>(objA: T, objB: U) {
+  return ({
+    ...objA,
+    ...objB,
+  })
+}
+
+const mergeObject = merge({ name: 'Max' }, { age: 26 })
+
+console.log(mergeObject.age)
+
