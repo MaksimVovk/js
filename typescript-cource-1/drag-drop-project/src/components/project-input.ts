26e8c92637d32a97469577618b7ab4d58b4ad9f4
MaximVovk <maksemvovk@gmail.com>
2021-03-13 17:46:32 +0200

    Using ES Modules

--- typescript-cource-1/drag-drop-project/src/components/project-input.ts
+++ typescript-cource-1/drag-drop-project/src/components/project-input.ts
@@ -1,84 +1,82 @@
-///<reference path="../util/validation.ts"/>
-///<reference path="../decorator/autobind.ts"/>
-///<reference path="../state/project-state.ts"/>
-///<reference path="./base-component.ts"/>
+import { Component } from './base-component.js'
+import { Autobind } from '../decorator/autobind.js'
+import { projectState } from '../state/project-state.js'
+import { validate, Validatable } from '../util/validation.js'
 
-namespace App {
-  //ProjectInput Class
-  export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement>{
-    titleInputElement: HTMLInputElement
-    descriptionInputElement: HTMLInputElement
-    peopleInputElement: HTMLInputElement
+//ProjectInput Class
+export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement>{
+  titleInputElement: HTMLInputElement
+  descriptionInputElement: HTMLInputElement
+  peopleInputElement: HTMLInputElement
 
-    constructor () {
-      super('project-input', 'app', true, 'user-input')
-      this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement
-      this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement
-      this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement
+  constructor () {
+    super('project-input', 'app', true, 'user-input')
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement
 
-      this.configure()
+    this.configure()
+  }
+
+  configure () {
+    this.element.addEventListener('submit', this.submitHandler)
+  }
+
+  renderContent() {}
+
+  private clearInput () {
+    const clearElement = (element: HTMLInputElement) => element.value = ''
+
+    clearElement(this.titleInputElement)
+    clearElement(this.descriptionInputElement)
+    clearElement(this.peopleInputElement)
+  }
+
+  @Autobind
+  private submitHandler (event: Event){
+    event.preventDefault()
+    const userInput = this.gatherUserInput()
+
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput
+      console.log(`title: ${title}; description: ${description}; people: ${people}`)
+
+      projectState.addProject(title, description, people)
+
+      this.clearInput()
+    }
+  }
+
+  private gatherUserInput (): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value
+    const enteredDescriptorTitle = this.descriptionInputElement.value
+    const enteredPeopleTitle = this.peopleInputElement.value
+
+    const tittleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true,
     }
 
-    configure () {
-      this.element.addEventListener('submit', this.submitHandler)
+    const descriptionValidatable: Validatable = {
+      value: enteredDescriptorTitle,
+      required: true,
+      minLength: 5,
     }
 
-    renderContent() {}
-
-    private clearInput () {
-      const clearElement = (element: HTMLInputElement) => element.value = ''
-
-      clearElement(this.titleInputElement)
-      clearElement(this.descriptionInputElement)
-      clearElement(this.peopleInputElement)
+    const peopleValidatable: Validatable = {
+      value: +enteredPeopleTitle,
+      required: true,
+      min: 2,
+      max: 5,
     }
 
-    @Autobind
-    private submitHandler (event: Event){
-      event.preventDefault()
-      const userInput = this.gatherUserInput()
+    const validateCondition = !validate(tittleValidatable) || !validate(descriptionValidatable) || !validate(peopleValidatable)
 
-      if (Array.isArray(userInput)) {
-        const [title, description, people] = userInput
-        console.log(`title: ${title}; description: ${description}; people: ${people}`)
-
-        projectState.addProject(title, description, people)
-
-        this.clearInput()
-      }
-    }
-
-    private gatherUserInput (): [string, string, number] | void {
-      const enteredTitle = this.titleInputElement.value
-      const enteredDescriptorTitle = this.descriptionInputElement.value
-      const enteredPeopleTitle = this.peopleInputElement.value
-
-      const tittleValidatable: Validatable = {
-        value: enteredTitle,
-        required: true,
-      }
-
-      const descriptionValidatable: Validatable = {
-        value: enteredDescriptorTitle,
-        required: true,
-        minLength: 5,
-      }
-
-      const peopleValidatable: Validatable = {
-        value: +enteredPeopleTitle,
-        required: true,
-        min: 2,
-        max: 5,
-      }
-
-      const validateCondition = !validate(tittleValidatable) || !validate(descriptionValidatable) || !validate(peopleValidatable)
-
-      if (validateCondition) {
-        alert('Invalid input')
-        return
-      } else {
-        return [enteredTitle, enteredDescriptorTitle, +enteredPeopleTitle]
-      }
+    if (validateCondition) {
+      alert('Invalid input')
+      return
+    } else {
+      return [enteredTitle, enteredDescriptorTitle, +enteredPeopleTitle]
     }
   }
 }
--- typescript-cource-1/drag-drop-project/src/decorator/autobind.ts
+++ typescript-cource-1/drag-drop-project/src/decorator/autobind.ts
@@ -1,16 +1,14 @@
-namespace App {
-  //autobind decorator
-  export function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
-    const originalMethod = descriptor.value
+//autobind decorator
+export function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value
 
-    const adjDescriptor: PropertyDescriptor = {
-      configurable: true,
-      get () {
-        const boundFn = originalMethod.bind(this)
-        return boundFn
-      }
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get () {
+      const boundFn = originalMethod.bind(this)
+      return boundFn
     }
-
-    return adjDescriptor
   }
+
+  return adjDescriptor
 }
