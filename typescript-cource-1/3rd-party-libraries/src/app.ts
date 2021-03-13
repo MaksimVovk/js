97f288e178d48d3e693b0509e768b072b8b390f0
MaximVovk <maksemvovk@gmail.com>
2021-03-13 22:06:10 +0200

    No Types Needed: class-transformer

--- typescript-cource-1/3rd-party-libraries/src/app.ts
+++ typescript-cource-1/3rd-party-libraries/src/app.ts
@@ -1,6 +1,25 @@
-import _ from 'lodash'
+// import _ from 'lodash'
 
-declare var GLOBAL: any
+// declare var GLOBAL: any
 
-console.log(GLOBAL)
-console.log(_.shuffle([1, 2, 3, 4]))
+// console.log(GLOBAL)
+// console.log(_.shuffle([1, 2, 3, 4]))
+import 'reflect-metadata'
+import { plainToClass } from 'class-transformer'
+import { Product } from './product.model'
+
+// const p1 = new Product('A Book', 12.99)
+// console.log(p1.getInformation())
+
+const products = [
+  { title: 'A Carpet', price: 29.99 },
+  { title: 'A Book', price: 12.99 }
+]
+
+// const loaderProducts = products.map(product => new Product(product.title, product.price))
+
+const loaderProducts = plainToClass(Product, products)
+
+for (const prod of loaderProducts) {
+  console.log(prod.getInformation())
+}
