3e5a33af038d36348fd8a9c8d12772c1841e525d
MaximVovk <maksemvovk@gmail.com>
2021-03-15 23:57:49 +0200

    Setting Up a Google API Key

--- typescript-cource-1/google-maps-app/src/app.ts
+++ typescript-cource-1/google-maps-app/src/app.ts
@@ -1,11 +1,14 @@
 const form = document.querySelector('form')!
 const addressInput = document.getElementById('address')! as HTMLInputElement
+const GOOGLE_API_KEY = 'AIzaSyCBXX75i_MPqdkEIZ75QBjsQx3MHQ2QEwE'
 
 function searchAddressHandler (event: Event) {
   event.preventDefault()
   const eneredAddress = addressInput.value
 
   //send this to Google API
+  //https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
+
 }
 
 form.addEventListener('submit', searchAddressHandler)
