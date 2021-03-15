448df42fb97323f1f096f5f5730b229c85f62323
MaximVovk <maksemvovk@gmail.com>
2021-03-16 00:23:29 +0200

    Using Axios to Fetch Coordinates for an Entered Address

--- typescript-cource-1/google-maps-app/src/app.ts
+++ typescript-cource-1/google-maps-app/src/app.ts
@@ -1,13 +1,43 @@
+import axios from 'axios'
+
 const form = document.querySelector('form')!
 const addressInput = document.getElementById('address')! as HTMLInputElement
 const GOOGLE_API_KEY = 'AIzaSyCBXX75i_MPqdkEIZ75QBjsQx3MHQ2QEwE'
+
+type GoogleGeocodingResponse = {
+  results: {
+    geometry: {
+      location: {
+        lat: number,
+        lng: number
+      }
+    }
+  }[]
+  status: 'OK' | 'ZERO_RESULTS'
+}
 
 function searchAddressHandler (event: Event) {
   event.preventDefault()
   const eneredAddress = addressInput.value
 
   //send this to Google API
-  //https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
+  const HOST = 'https://maps.googleapis.com/maps/api/geocode/json'
+  const ADDRESS = 'address=' + encodeURI(eneredAddress)
+  const API_KEY = 'key=' + GOOGLE_API_KEY
+
+  axios.get<GoogleGeocodingResponse>(HOST + '?' + ADDRESS + '&' + API_KEY)
+  .then(r => {
+    if (r.data.status !== 'OK') {
+      throw new Error('Could not fetch location!')
+    }
+
+    const coordinates = r.data?.results[0].geometry.location
+    console.log(coordinates)
+  })
+  .catch(e => {
+    alert(e.message)
+    console.log(e)
+  })
 
 }
 
