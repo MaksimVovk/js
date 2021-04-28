d9831d8e43a73dcf35569cb969391797b144072c
MaximVovk <maksemvovk@gmail.com>
2021-04-28 21:39:55 +0300

    python: if-elif-else conditions example

--- python/HelloWorld/app.py
+++ python/HelloWorld/app.py
@@ -1,1 +1,20 @@
-print('Max')
+name = 'John Smith'
+age = 20
+isNewPatient = False
+
+if name == 'John Smith':
+    print('It is John Smith')
+else:
+    print('It is not John Smith')
+
+if age == 20:
+    print('He is 20 years old')
+elif age > 20:
+    print('He is older')
+else:
+    print('He is younger')
+
+if isNewPatient:
+    print('He is a new patient')
+else:
+    print('He is a current patient')
