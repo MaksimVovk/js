d0071b448402e92148001c31d0b4e573089bd839
MaximVovk <maksemvovk@gmail.com>
2021-05-03 20:57:59 +0300

    Classes

---
+++ python/HelloWorld/classes.py
@@ -0,0 +1,16 @@
+import sys
+
+
+class Point:
+    def move(self):
+        print("Move")
+
+    def draw(self):
+        print("Draw")
+
+
+point1 = Point()
+point1.x = 10
+point1.y = 20
+print(point1.y)
+point1.draw()
--- python/HelloWorld/function.py
+++ python/HelloWorld/function.py
@@ -1,5 +1,4 @@
 import sys
-
 # Exceptions
 try:
     age = int(input('Age: '))
