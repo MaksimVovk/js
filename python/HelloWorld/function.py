f3587d0c9f6bd52eca4bc7def2e0762e51b18068
MaximVovk <maksemvovk@gmail.com>
2021-05-03 20:11:40 +0300

    python: Return Statement

--- python/HelloWorld/function.py
+++ python/HelloWorld/function.py
@@ -1,7 +1,14 @@
 import sys
+# Return Statement
+def square(number):
+    return number * number
+
+
+result = square(4)
+print(result)
+sys.exit()
+
 # functions
-
-
 def greet_user(first_name="Gomer", last_name="Simpson"):
     print(f'Hi there {first_name} {last_name}')
     print('Welcome aboard')
