437b413c9ed62e9075f68d2e4c26c21c54795a6f
MaximVovk <maksemvovk@gmail.com>
2021-05-03 14:58:26 +0300

    python: If Statements

--- python/HelloWorld/app.py
+++ python/HelloWorld/app.py
@@ -1,6 +1,19 @@
 import sys
+# If Statements
+is_hot = False
+is_cold = True
+if is_hot:
+    print("It's a hot day")
+    print("Drink plenty of watter")
+elif is_cold:
+    print("It's a cold day")
+    print("Wear warm clothes")
+else:
+    print("It's a lovely day")
+print("Enjoy your day")
+sys.exit()
 
-#Math Functions
+# Math Functions
 import math
 x = 2.9
 print(round(x))
