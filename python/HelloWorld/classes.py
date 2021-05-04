079280c1c59ffc3ec91778af1976d175d91cc487
MaximVovk <maksemvovk@gmail.com>
2021-05-04 17:34:24 +0300

    python: Constructors

--- python/HelloWorld/classes.py
+++ python/HelloWorld/classes.py
@@ -1,6 +1,35 @@
 import sys
 
+class Person:
+    def __init__(self, name):
+        self.name = name
+    
+    def talk(self):
+        print(f'{self.name} is talk now')
 
+person = Person('Max')
+person.talk()
+sys.exit()
+# Constructors
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move(self):
+        print("Move")
+
+    def draw(self):
+        print("Draw")
+
+
+point = Point(10, 20)
+point.x = 11
+print(point.x)
+
+
+sys.exit()
+# Classes
 class Point:
     def move(self):
         print("Move")
