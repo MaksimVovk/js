4fcde8516ddcf57092773c0dea03e7fa099d4bcd
MaximVovk <maksemvovk@gmail.com>
2021-06-06 15:01:27 +0300

    Anonymous Functions

--- flutter/flutter_complete_guid/lib/main.dart
+++ flutter/flutter_complete_guid/lib/main.dart
@@ -6,14 +6,38 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My first app'),
         ),
-        body: Text('This is my default text!'),
+        body: Column(
+          children: [
+            Text('The question!'),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: () => answerQuestion(),
+            ),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: () => answerQuestion(),
+            ),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: () => answerQuestion(),
+            ),
+          ],
+        ),
       ),
     );
   }
