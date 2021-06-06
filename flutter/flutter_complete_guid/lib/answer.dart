564e287200d97612a005ff059d98ca1b0dc68e96
MaximVovk <maksemvovk@gmail.com>
2021-06-06 22:06:17 +0300

    "Old" Button Widgets vs "New" Button

--- flutter/flutter_complete_guid/lib/answer.dart
+++ flutter/flutter_complete_guid/lib/answer.dart
@@ -2,18 +2,24 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
   final Function selectHandler;
-  var answer;
+  final answer;
 
   Answer({this.selectHandler, this.answer});
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      child: RaisedButton(
-        color: Colors.blue,
-        textColor: Colors.white,
+      child: ElevatedButton(
         child: Text(answer),
         onPressed: selectHandler,
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all(Colors.white),
+          backgroundColor: MaterialStateProperty.all(Colors.blue),
+        ),
+        // style: ElevatedButton.styleFrom(
+        //   primary: Colors.orange,
+        //   onPrimary: Colors.white,
+        // ),
       ),
     );
   }
