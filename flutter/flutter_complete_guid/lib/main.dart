acf3b5b5be38fca25d58d9bd820c615529d7390b
MaximVovk <maksemvovk@gmail.com>
2021-06-06 19:35:23 +0300

    Outputting Widgets Conditionally

--- flutter/flutter_complete_guid/lib/main.dart
+++ flutter/flutter_complete_guid/lib/main.dart
@@ -10,7 +10,6 @@ import './answer.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
 
@@ -19,6 +18,21 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'question': 'What\'s your favorite color?',
+      'answers': ['Black','Red','White','Green'],
+    },
+    {
+      'question': 'What\'s your favorite animal?',
+      'answers': ['Rabbit', 'Snake', 'Lion', 'Elephant'],
+    },
+    {
+      'question': 'Who\'s your favorite instructor?',
+      'answers': ['Max', 'Max', 'Max', 'Max'],
+    },
+  ];
+
   int _questionIndex = 0;
 
   void _answerQuestion() {
@@ -26,43 +40,39 @@ class _MyAppState extends State<MyApp> {
       _questionIndex = _questionIndex + 1;
     });
     print(_questionIndex);
+
+    if (_questionIndex < questions.length) {
+      print('We have more questions!');
+    } else {
+      print('We don\'t have any questions!');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'question': 'What\'s your favorite color?',
-        'answers': ['Black','Red','White','Green'],
-      },
-      {
-        'question': 'What\'s your favorite animal?',
-        'answers': ['Rabbit', 'Snake', 'Lion', 'Elephant'],
-      },
-      {
-        'question': 'Who\'s your favorite instructor?',
-        'answers': ['Max', 'Max', 'Max', 'Max'],
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My first app'),
         ),
-        body: Column(
-          children: [
-            Question(
-              questions[_questionIndex]['question'],
-            ),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-            .map((it) {
-              return Answer(
-                answer: it,
-                selectHandler: _answerQuestion
-              );
-            }).toList(),
-          ],
-        ),
+        body: _questionIndex < questions.length
+          ? Column(
+            children: [
+              Question(
+                questions[_questionIndex]['question'],
+              ),
+              ...(questions[_questionIndex]['answers'] as List<String>)
+              .map((it) {
+                return Answer(
+                  answer: it,
+                  selectHandler: _answerQuestion
+                );
+              }).toList(),
+            ],
+          )
+          : Center(
+            child: Text('You did it!'),
+          ),
       ),
     );
   }
