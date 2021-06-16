458a7ae5eaa4dafbf59e4a2451401bd3e20ddba8
MaximVovk <maksemvovk@gmail.com>
2021-06-16 10:35:38 +0300

    Making the List Scrollable!

--- flutter/personal_expenses_app/lib/widgets/transaction_list.dart
+++ flutter/personal_expenses_app/lib/widgets/transaction_list.dart
@@ -8,52 +8,57 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: userTransaction.map((tx) {
-        return Card(
-          child: Row(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                child: Text(
-                  'A: \$${tx.amount}',
-                  style: TextStyle(
-                    color: Colors.purple,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ),
-                ),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.purple,
-                    width: 2,
-                  )
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: userTransaction.map((tx) {
+            return Card(
+              child: Row(
                 children: <Widget>[
-                  Text(
-                    tx.title,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                    child: Text(
+                      'A: \$${tx.amount}',
+                      style: TextStyle(
+                        color: Colors.purple,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      )
                     ),
                   ),
-                  Text(
-                    DateFormat('yyyy-MM-dd H:m').format(tx.date),
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 14
-                    ),
-                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        tx.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      Text(
+                        DateFormat('yyyy-MM-dd H:m').format(tx.date),
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 14
+                        ),
+                      ),
+                    ],
+                  )
                 ],
               )
-            ],
-          )
-        );
-      }).toList(),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
