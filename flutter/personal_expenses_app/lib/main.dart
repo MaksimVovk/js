4f25d2849e6f30cc6acc64c43b36fd48681d61c5
MaximVovk <maksemvovk@gmail.com>
2021-06-23 00:14:53 +0300

    Calculating Sizes Dynamically

--- flutter/personal_expenses_app/lib/main.dart
+++ flutter/personal_expenses_app/lib/main.dart
@@ -121,26 +121,42 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final appBar = AppBar(
+      title: Text('Flutter App'),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () => _startAddNewTransaction(context),
+        )
+      ],
+    );
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () => _startAddNewTransaction(context),
-          )
-        ],
-      ),
+      appBar: appBar,
       body:
       SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Chart(_resentTransactions),
-            TransactionList(
-              userTransaction: _userTransaction,
-              deleteTransaction: _deleteTransaction,
+            Container(
+              height: (
+                MediaQuery.of(context).size.height
+                - appBar.preferredSize.height
+                - MediaQuery.of(context).padding.top
+              ) * 0.4,
+              child: Chart(_resentTransactions),
+            ),
+            Container(
+              height: (
+                MediaQuery.of(context).size.height
+                - appBar.preferredSize.height
+                - MediaQuery.of(context).padding.top
+              ) * 0.6,
+              child: TransactionList(
+                userTransaction: _userTransaction,
+                deleteTransaction: _deleteTransaction,
+              ),
             ),
           ],
         ),
--- flutter/personal_expenses_app/lib/widgets/transaction_list.dart
+++ flutter/personal_expenses_app/lib/widgets/transaction_list.dart
@@ -10,57 +10,54 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 400,
-      child: userTransaction.isEmpty
-        ? Column(
-          children: <Widget>[
-            Text(
-              'No transactions!',
-              style: Theme.of(context).textTheme.title,
+    return userTransaction.isEmpty
+      ? Column(
+        children: <Widget>[
+          Text(
+            'No transactions!',
+            style: Theme.of(context).textTheme.title,
+          ),
+          SizedBox(height: 10),
+          Container(
+            height: 200,
+            child: Image.asset('assets/images/waiting.png', fit: BoxFit.cover)
+          ),
+        ]
+      )
+      : ListView.builder(
+        itemBuilder: (ctx, index) {
+          return Card(
+            elevation: 5,
+            margin: EdgeInsets.symmetric(
+              vertical: 8,
+              horizontal: 5,
             ),
-            SizedBox(height: 10),
-            Container(
-              height: 200,
-              child: Image.asset('assets/images/waiting.png', fit: BoxFit.cover)
-            ),
-          ]
-        )
-        : ListView.builder(
-          itemBuilder: (ctx, index) {
-            return Card(
-              elevation: 5,
-              margin: EdgeInsets.symmetric(
-                vertical: 8,
-                horizontal: 5,
-              ),
-              child: ListTile(
-                leading: CircleAvatar(
-                  radius: 30,
-                  child: Padding(
-                    padding: EdgeInsets.all(6),
-                    child: FittedBox(
-                      child: Text('\$${userTransaction[index].amount}'),
-                    )
+            child: ListTile(
+              leading: CircleAvatar(
+                radius: 30,
+                child: Padding(
+                  padding: EdgeInsets.all(6),
+                  child: FittedBox(
+                    child: Text('\$${userTransaction[index].amount}'),
                   )
-                ),
-                title: Text(
-                  userTransaction[index].title,
-                  style: Theme.of(context).textTheme.title,
-                ),
-                subtitle: Text(
-                  DateFormat.yMMMd().format(userTransaction[index].date),
-                ),
-                trailing: IconButton(
-                  icon: Icon(Icons.delete),
-                  color: Theme.of(context).errorColor,
-                  onPressed: () => deleteTransaction(userTransaction[index].id),
-                ),
-              )
-            );
-          },
-          itemCount: userTransaction.length,
-        ),
-    );
+                )
+              ),
+              title: Text(
+                userTransaction[index].title,
+                style: Theme.of(context).textTheme.title,
+              ),
+              subtitle: Text(
+                DateFormat.yMMMd().format(userTransaction[index].date),
+              ),
+              trailing: IconButton(
+                icon: Icon(Icons.delete),
+                color: Theme.of(context).errorColor,
+                onPressed: () => deleteTransaction(userTransaction[index].id),
+              ),
+            )
+          );
+        },
+        itemCount: userTransaction.length,
+      );
   }
 }
