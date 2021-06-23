3918301240ac9ecc4633be2dcc3a4727b419dc36
MaximVovk <maksemvovk@gmail.com>
2021-06-23 10:08:54 +0300

    Using the Device Size in Conditions

--- flutter/personal_expenses_app/lib/widgets/new_transaction.dart
+++ flutter/personal_expenses_app/lib/widgets/new_transaction.dart
@@ -63,7 +63,7 @@ class _NewTransactionState extends State<NewTransaction> {
             top: 5,
             left: 10,
             right: 10,
-            bottom: MediaQuery.of(context).viewInsets.bottom - 91,
+            // bottom: MediaQuery.of(context).viewInsets.bottom + 10
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.end,
