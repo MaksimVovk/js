ca5da43a05f7c43c3f046c6996425362a3350af4
MaximVovk <maksemvovk@gmail.com>
2021-07-22 10:20:29 +0300

    Adding Orders

--- flutter/shop_app/lib/screens/cart_screen.dart
+++ flutter/shop_app/lib/screens/cart_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/cart.dart' show Cart;
 import 'package:shop_app/widgets/cart_item.dart';
+import 'package:shop_app/providers/orders.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -41,8 +42,16 @@ class CartScreen extends StatelessWidget {
                     backgroundColor: Theme.of(context).primaryColor,
                   ),
                   TextButton(
-                    onPressed: (){},
+                    onPressed: (){
+                      Provider.of<Orders>(context, listen: false)
+                      .addOrder(
+                        cart.items.values.toList(),
+                        cart.totalAmount
+                      );
+                      cart.clear();
+                    },
                     child: Text('Order now'),
+
                   )
                 ],
               ),
